f14d4861e5036301b11a0a21f74b8038d0a43658
Student <cesarezequielaguero222gmail.com>
2024-02-07 18:04:12 +0100

    mmm

---
+++ carpeta trabajo/biblioteca.py
@@ -0,0 +1,29 @@
+# import sys
+
+# help ("sys")
+# dir(sys)
+# python standart library = para encontrar modulos 
+# import math as m
+
+# x= 10.257
+
+# print(m.ceil(x))
+# print(m.floor(x))
+# from math import ceil, floor
+# x= 10.257
+
+# print(ceil(x))
+# print(floor(x))
+# import random
+
+# print(random.random())
+
+
+import random as ran
+
+# print(ran.randint(5, 100))
+
+# print(ran.choice([3,6,9, "hola"]))
+
+# print(ran.shuffle(["lksjd, ald, laj"]))
+
