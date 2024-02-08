4658525374ba43c56de2f591cbc28e7c4b5dbdb2
Student <cesarezequielaguero222@gmail.com>
2024-02-08 16:05:13 +0100

    repaso

---
+++ carpeta trabajo/repaso2.py
@@ -0,0 +1,17 @@
+# import random
+# cartas= ["a","b","c","d"]
+
+# opcion= random.choice(cartas)
+# print(opcion)
+
+# random.shuffle(cartas)
+# print(cartas)
+
+# import datetime
+
+# print(datetime.datetime.now()) primera forma de importar datetime
+
+# from datetime import datetime
+
+# print(datetime.now()) segunda forma de importar datetime
+
