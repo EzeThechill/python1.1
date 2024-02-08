24238f344d43bc8f4a07b9c0d132cd837958056f
Student <cesarezequielaguero222@gmail.com>
2024-02-08 17:04:56 +0100

    fgh

--- carpeta trabajo/repaso2.py
+++ carpeta trabajo/repaso2.py
@@ -15,3 +15,26 @@
 
 # print(datetime.now()) segunda forma de importar datetime
 
+# import random as ran 
+
+# raton=1
+# prueba=[1,2,3,4,5,6,7,8,9]
+
+# while raton>prueba:
+#     if raton ==10
+
+# # i = 0
+# # while i<10:
+# #     if i == 5:
+# #         break
+# #     else:
+# #         print(i)
+# #     i = i+1
+
+
+
+# print(ram.random(prueba))    
+
+import webbrowser
+webbrowser.open("http://www.google.com")
+
