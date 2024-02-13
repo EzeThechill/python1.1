269a1c551567d0ea764b9dfd47af8f6979593e51
Student <cesarezequielaguero222@gmail.com>
2024-02-13 15:38:18 +0100

    nuevo

---
+++ repaso3.py
@@ -0,0 +1,13 @@
+s="    122,Python,es,64,un,777,lenguaje,222,de,55,66,programación   "
+
+
+s= s.strip()
+listastring = s.split(",")
+listadepalabras = []
+for i in listastring:
+    if not i.isnumeric():
+        listadepalabras.append(i)
+
+resultado = ",".join(listadepalabras)
+print(resultado)
+
