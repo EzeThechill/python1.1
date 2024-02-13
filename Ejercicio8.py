4d1ae3c3fa1accf0ddb43873056959dee8238024
Student <cesarezequielaguero222@gmail.com>
2024-02-13 17:13:56 +0100

    fj

---
+++ Ejercicio8.py
@@ -0,0 +1,51 @@
+# Actividades
+# Usando replace(), cambiar el texto para que sea correcto. Además, usar el nombre original de “Python” en lugar de Píton.
+
+# texto = "Pitón es un lenguaje de programación. Pitón está usado para la automación, análisis de datos e incluso la creación de páginas webs. Pitón fue creado por Bill Gates en 1960. Pitón es difícil de usar."
+
+# texto = "Pitón es un lenguaje de programación. Pitón está usado para la automación, análisis de datos e incluso la creación de páginas webs. Pitón fue creado por Bill Gates en 1960. Pitón es difícil de usar."
+
+# texto = texto.replace("Pitón", "Python")
+# print(texto)
+
+# # Usando este texto, llevar a cabo:
+
+# # texto = "    Lo más importante que nos ha mantenido en Python... bueno, hay 2 cosas importantes. Uno son las bibliotecas. La otra cosa que nos mantiene en Python, y esto es lo más importante, es facil de leer y entender. Cuando contratamos nuevos empleados. Solo digo, 'todo lo que escribas debe estar en python'. Sólo para que pueda leerlo. Y es increíble porque puedo ver desde el otro lado de la habitación, mirando su pantalla, si su código es bueno o malo. Porque un buen código de Python tiene una estructura muy obvia. Y eso hace que mi vida sea mucho más fácil        "
+
+# texto = "    Lo más importante que nos ha mantenido en Python... bueno, hay 2 cosas importantes. Uno son las bibliotecas. La otra cosa que nos mantiene en Python, y esto es lo más importante, es facil de leer y entender. Cuando contratamos nuevos empleados. Solo digo, 'todo lo que escribas debe estar en python'. Sólo para que pueda leerlo. Y es increíble porque puedo ver desde el otro lado de la habitación, mirando su pantalla, si su código es bueno o malo. Porque un buen código de Python tiene una estructura muy obvia. Y eso hace que mi vida sea mucho más fácil        "
+
+# texto = texto.strip()
+# print(texto)
+
+# Usar swapcase, replace, upper, in, count, find, strip
+
+# Contar las veces que la palabra Python aparece en el texto...y si a veces aparece en el texto con mayusculas y minusculas - Python, python
+# Encontrar la ubicación (numero/indice de carácter) donde esta la primera ocurrencia de la palabra Python. ¿Y la segunda?
+# ¿La palabra 'código' está en el texto? Usar if ... in ...:
+# Reemplazar Python por PYTHON
+# Quitar los espacios al inicio/final
+# Cambiar la letra de todo el texto a "lO MÁS IMPORTANTE QUE NOS HA MANTENIDO EN pYTHON... " - no usar replace
+
+# texto = "    Lo más importante que nos ha mantenido en Python... bueno, hay 2 cosas importantes. Uno son las bibliotecas. La otra cosa que nos mantiene en Python, y esto es lo más importante, es facil de leer y entender. Cuando contratamos nuevos empleados. Solo digo, 'todo lo que escribas debe estar en python'. Sólo para que pueda leerlo. Y es increíble porque puedo ver desde el otro lado de la habitación, mirando su pantalla, si su código es bueno o malo. Porque un buen código de Python tiene una estructura muy obvia. Y eso hace que mi vida sea mucho más fácil        "
+
+# texto = texto.strip()
+# # texto1 = texto.upper()
+# contar = texto.count("Python", "python", "PYTHON")
+
+# for texto in contar range:
+    
+
+# print(texto)
+
+
+
+# Gestión de los correos electrónicos
+# emails = ["jon.smith@microsoft.com", "maria.fernandez@microsoft.com", "david@microsoft.com", "isabel@microsoft.es","alfonso@gmail.com"]
+
+# # Generar un informe con:
+# # - los nombres de los usuarios
+# # - los dominios únicos (sin repetir el dominio)
+
+# nombres = ("maria", "jon", "david")
+# # con los nombres, generar un texto en formato CSV con nuevos correos e.g maria@nazaret.eus,jon@nazaret.eus,david@nazaret.eus
+
