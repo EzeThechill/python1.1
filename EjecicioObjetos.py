0758ebb401243b0a208398bd786e0efcd6d5ac3d
Student <cesarezequielaguero222@gmail.com>
2024-02-28 17:56:37 +0100

    qe

--- EjecicioObjetos.py
+++ EjecicioObjetos.py
@@ -1,24 +1,24 @@
-class Guitarra:
-    def __init__(self, marca, cuerdas): 
-        self.marca = marca
-        self.cuerdas = cuerdas
-        self._precio = 100
+# class Guitarra:
+#     def __init__(self, marca, cuerdas): 
+#         self.marca = marca
+#         self.cuerdas = cuerdas
+#         self._precio = 100
     
-    def tocar(self):
-        print(f"Soy {self.marca} y brrn, brannn, bromm")
+#     def tocar(self):
+#         print(f"Soy {self.marca} y brrn, brannn, bromm")
 
 # main programa - instanciar / usar la clase
         
-nombre = input("Cual es el nombre de la guitarra")
-guit1 = Guitarra("les paul", 6)
-print(guit1.marca)
-print(guit1.cuerdas)
+# nombre = input("Cual es el nombre de la guitarra")
+# guit1 = Guitarra("les paul", 6)
+# print(guit1.marca)
+# print(guit1.cuerdas)
 
 # bajo = Guitarra("bajo secillo", 4)
 # bajo.cuerdas=5
 # print(bajo.cuerdas)
 
-guit1.tocar()
+# guit1.tocar()
 
 # class Taza:
 #     def __init__(self, liquido, cantidad):
@@ -28,10 +28,32 @@ guit1.tocar()
 #     def beber(self):
 #         print(f"que quieres que {self.liquido} ")
 
+# ----------------------------
 
 # pedido = input("Que quieres que te prepare ")
 # taza1 = Taza("cafe con leche", 10)
 # print()
 
-        
+#---------------------------------
 
+class MaquinaExpendedora:
+    def __init__(self, tipo, volumen, cantidad):
+        self.tipo = tipo
+        self.volumen = volumen
+        self.coste = cantidad
+
+        def beber(self):
+            print(f"que quieres que {self.tipo} ")
+
+listaBebidas = []
+
+for i in range(3):
+    pedido = input("Que quieres que te prepare ")
+    cuanto = input("Cuanto vas a beber ahora (0 a 100)? ")
+    bebida = bebida(pedido, cuanto, 100)
+    listaBebidas.append(bebida)
+
+for bebida in listaBebidas:
+    print(bebida.tipo)
+    print(bebida.cantidad)
+    print(bebida) #__str__
