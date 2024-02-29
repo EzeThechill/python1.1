4d195c1db9df505431933592048598f786a8b080
Student <cesarezequielaguero222@gmail.com>
2024-02-29 16:02:26 +0100

    klj

--- EjecicioObjetos.py
+++ EjecicioObjetos.py
@@ -1,34 +1,46 @@
-# class Guitarra:
-#     def __init__(self, marca, cuerdas): 
-#         self.marca = marca
-#         self.cuerdas = cuerdas
-#         self._precio = 100
+class Guitarra:
+    def __init__(self, marca, cuerdas=6): 
+        self.marca = marca
+        self.cuerdas = cuerdas
+        self._precio = 100
     
-#     def tocar(self):
-#         print(f"Soy {self.marca} y brrn, brannn, bromm")
+    def romperCuerdas(self, cuerdasRotas):
+        if cuerdasRotas > self.cuerdas:
+            self.cuerdas = 0
+            print("no se puede romper mas de 1")
+        else:
+            self.cuerdas = self.cuerdas - cuerdasRotas
+            print(f"Me he quedado con {self.cuerdas} cuerdas ")
+            
+    def __str__(self):
+        return f"Hola Guitarra {self.marca}"
+     
+    def tocar(self):
+        if self.cuerdas > 0:
+            print(f"Soy {self.marca} y brrn, brannn, bromm")
+        else:
+            print("Losiento no tengo cuerdas")
 
 # main programa - instanciar / usar la clase
         
 # nombre = input("Cual es el nombre de la guitarra")
-# guit1 = Guitarra("les paul", 6)
-# print(guit1.marca)
-# print(guit1.cuerdas)
+guit = Guitarra("les paul", 6)
+print(guit.cuerdas)
+guit.romperCuerdas(1)
+print(guit.cuerdas)
+guit.tocar()
 
-# bajo = Guitarra("bajo secillo", 4)
-# bajo.cuerdas=5
-# print(bajo.cuerdas)
-
-# guit1.tocar()
 
 # class Taza:
-#     def __init__(self, liquido, cantidad):
+#     def __init__(self, liquido, cantidad=100):
 #         self.liquido = liquido
 #         self.cantidad = cantidad
     
-#     def beber(self):
+#     def beber(self, berberLiquido):
+#         self.cantidad = self.cantidad - berberLiquido
 #         print(f"que quieres que {self.liquido} ")
 
-# ----------------------------
+# # ----------------------------
 
 # pedido = input("Que quieres que te prepare ")
 # taza1 = Taza("cafe con leche", 10)
@@ -36,24 +48,24 @@
 
 #---------------------------------
 
-class MaquinaExpendedora:
-    def __init__(self, tipo, volumen, cantidad):
-        self.tipo = tipo
-        self.volumen = volumen
-        self.coste = cantidad
+# class MaquinaExpendedora:
+#     def __init__(self, tipo, volumen, cantidad):
+#         self.tipo = tipo
+#         self.volumen = volumen
+#         self.coste = cantidad
 
-        def beber(self):
-            print(f"que quieres que {self.tipo} ")
+#         def beber(self):
+#             print(f"que quieres que {self.tipo} ")
 
-listaBebidas = []
+# listaBebidas = []
 
-for i in range(3):
-    pedido = input("Que quieres que te prepare ")
-    cuanto = input("Cuanto vas a beber ahora (0 a 100)? ")
-    bebida = bebida(pedido, cuanto, 100)
-    listaBebidas.append(bebida)
+# for i in range(3):
+#     pedido = input("Que quieres que te prepare ")
+#     cuanto = input("Cuanto vas a beber ahora (0 a 100)? ")
+#     bebida = bebida(pedido, cuanto, 100)
+#     listaBebidas.append(bebida)
 
-for bebida in listaBebidas:
-    print(bebida.tipo)
-    print(bebida.cantidad)
-    print(bebida) #__str__
+# for bebida in listaBebidas:
+#     print(bebida.tipo)
+#     print(bebida.cantidad)
+#     print(bebida) #__str__
