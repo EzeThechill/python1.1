a2dece17ea6e8b1208b99fcb7cccdb8f42c63577
Student <cesarezequielaguero222@gmail.com>
2024-03-06 17:09:17 +0100

    IKY

--- SQL.py
+++ SQL.py
@@ -1,4 +1,9 @@
 import sqlite3
 conn = sqlite3.connect('pelis.db')
-conn.execute("CREATE TABLE Peliculas (id integer, nombre text, duracion integer)")
+#conn.execute("CREATE TABLE Peliculas (id integer, nombre text, duracion integer)")
+sData =[(1,"Tiburon",3),(2,"Jones",2)]
+
+cur = conn.cursor()
+cur.executemany("INSERT INTO Peliculas (id, nombre, duracion)VALUES (?, ?, ?);", sData)
+
 conn.close
