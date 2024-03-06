77a7e0ba6d94a77843bbdd9fd3c09409713421eb
Student <cesarezequielaguero222@gmail.com>
2024-03-06 17:52:12 +0100

    DSH

--- SQL.py
+++ SQL.py
@@ -1,9 +1,34 @@
+# import sqlite3
+# conn = sqlite3.connect('pelis.db')
+# #conn.execute("CREATE TABLE Peliculas (id integer, nombre text, duracion integer)")
+# sData =[(1,"Tiburon",3),(2,"Jones",2), (2, "Coste",1)]
+
+# nombre= input("¿Que peli?")
+# cur = conn.cursor()
+# cur.executemany("INSERT INTO Peliculas (id, nombre, duracion)VALUES (?, ?, ?);", sData)
+# sSQL = f"INSERT INTO Peliculas(id, nombre, duracion)VALUES (?, ?, ?);"
+# conn.execute(sSQL, (3,nombre, 2))
+# # conn.rollback() tirar para atras
+# # conn.commit()guardar los cambios
+# conn.commit()
+
+# conn.close
+
 import sqlite3
-conn = sqlite3.connect('pelis.db')
-#conn.execute("CREATE TABLE Peliculas (id integer, nombre text, duracion integer)")
-sData =[(1,"Tiburon",3),(2,"Jones",2)]
+COL_ID=0
+COL_NOMBRE=1
+COL_DURACION=2
+
+conn= sqlite3.connect('pelis.db')
 
 cur = conn.cursor()
-cur.executemany("INSERT INTO Peliculas (id, nombre, duracion)VALUES (?, ?, ?);", sData)
 
-conn.close
+cur.execute("SELECT * FROM Peliculas;")
+
+rows =cur.fetchall()
+
+for row in rows:
+    # print(type(row)) #(2, 'indiana jones', 2)
+    print((row[COL_DURACION])) # lista
+
+conn.close()
