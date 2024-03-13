dae13daeb6c777275222408588ebc8ebdd7f8f4f
Student <csarezequielaguero222@gmail.com>
2024-03-13 15:26:29 +0100

    h

--- repasoq.py
+++ repasoq.py
@@ -1,4 +1,14 @@
 productos = ["raqueta","balon", "guantes", "pelota"]
 
-def mayuscula(m):
-    
+captext = lambda x:x.capitalize()
+print(captext("raqueta"))
+
+def convertir_m(s):
+    return s.capitalize()
+
+
+for p in productos:
+    print(convertir_m(p))
+
+nuevap = [convertir_m(p) for p in productos]
+print(nuevap)
