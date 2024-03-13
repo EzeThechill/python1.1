1bf9724f425376620efb24e32d8b8b2e3edab1db
Student <csarezequielaguero222@gmail.com>
2024-03-13 15:30:38 +0100

    k

--- repasoq.py
+++ repasoq.py
@@ -1,14 +1,17 @@
 productos = ["raqueta","balon", "guantes", "pelota"]
 
-captext = lambda x:x.capitalize()
-print(captext("raqueta"))
+
 
 def convertir_m(s):
     return s.capitalize()
 
+print(list(map(convertir_m, productos)))
 
 for p in productos:
     print(convertir_m(p))
 
 nuevap = [convertir_m(p) for p in productos]
 print(nuevap)
+
+captext = lambda x:x.capitalize()
+print(captext("raqueta"))
